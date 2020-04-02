66dfa7c637e3ccea51a65a304d05e63cbfec02d1
emrizkiem <mr.hobskoy@gmail.com>
2020-04-02 07:24:43 +0700

    Update HomeViewModel :kissing_heart:

--- app/src/main/java/dev/emrizkiem/myblog/ui/home/HomeViewModel.java
+++ app/src/main/java/dev/emrizkiem/myblog/ui/home/HomeViewModel.java
@@ -3,29 +3,27 @@ package dev.emrizkiem.myblog.ui.home;
 import android.app.Application;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import dev.emrizkiem.myblog.data.model.Post;
+import dev.emrizkiem.myblog.data.repository.PostRepository;
 
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private PostRepository postRepository;
 
     public HomeViewModel(Application mApplication) {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
-    }
-
-    public LiveData<String> getText() {
-        return mText;
+        postRepository = new PostRepository(mApplication);
     }
 
     public LiveData<List<Post>> getPost() {
-        MutableLiveData<List<Post>> ldPostList = new MutableLiveData<>();
+        return postRepository.getAllPost();
+    }
+
+    public List<Post> getMock() {
         List<Post> postList = new ArrayList<>();
         Post post = new Post();
         post.setUsername("emrizkiem");
@@ -33,7 +31,6 @@ public class HomeViewModel extends ViewModel {
         post.setImagePath("https://avatars0.githubusercontent.com/u/26673503?s=400&u=0e18d02456aba966bb18f7724aaf82c49d7de451&v=4");
         post.setAvatarPath("https://avatars0.githubusercontent.com/u/26673503?s=400&u=0e18d02456aba966bb18f7724aaf82c49d7de451&v=4");
         postList.add(post);
-        ldPostList.setValue(postList);
-        return ldPostList;
+        return postList;
     }
 }
