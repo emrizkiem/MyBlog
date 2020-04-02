47739494c13c641d285cfa8243220d7f84f32f02
emrizkiem <mr.hobskoy@gmail.com>
2020-04-02 07:43:05 +0700

    Update HomeViewModel for create data :kissing_heart:

--- app/src/main/java/dev/emrizkiem/myblog/ui/home/HomeViewModel.java
+++ app/src/main/java/dev/emrizkiem/myblog/ui/home/HomeViewModel.java
@@ -17,13 +17,14 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel(Application mApplication) {
         postRepository = new PostRepository(mApplication);
+        postRepository.insert(getMockList());
     }
 
     public LiveData<List<Post>> getPost() {
         return postRepository.getAllPost();
     }
 
-    public List<Post> getMock() {
+    public List<Post> getMockList() {
         List<Post> postList = new ArrayList<>();
         Post post = new Post();
         post.setUsername("emrizkiem");
@@ -32,5 +33,14 @@ public class HomeViewModel extends ViewModel {
         post.setAvatarPath("https://avatars0.githubusercontent.com/u/26673503?s=400&u=0e18d02456aba966bb18f7724aaf82c49d7de451&v=4");
         postList.add(post);
         return postList;
+    }
+
+    public Post getMock() {
+        Post post = new Post();
+        post.setUsername("emrizkiem");
+        post.setCaption("Don't be shy to mistake because learn from mistake");
+        post.setImagePath("https://avatars0.githubusercontent.com/u/26673503?s=400&u=0e18d02456aba966bb18f7724aaf82c49d7de451&v=4");
+        post.setAvatarPath("https://avatars0.githubusercontent.com/u/26673503?s=400&u=0e18d02456aba966bb18f7724aaf82c49d7de451&v=4");
+        return post;
     }
 }
